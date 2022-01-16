ac61528c92fbdca35cf859d75d411ada17ad4abb
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2022-01-15 19:02:53 -0700

    if, elif, else

---
+++ if_elif_else.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+
+if name == "Tim":
+    print("Your name is Tim.")
+elif name == "Sarah":
+    print("Your name is Sarah.")
+else:
+    print("I don't care who you are.")
